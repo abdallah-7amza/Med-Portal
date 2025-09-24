9d2e19b16db393c7ec3adc8be4c6c9cc035a404b
abdallah-7amza <abdallah7amza@gmail.com>
2025-09-24 07:52:48 -0400

    Update index.js

--- docs/js/index.js
+++ docs/js/index.js
@@ -2,8 +2,8 @@
 /* eslint-disable no-undef */
 
 const contentPath = '../content/universities/nub';
-const versionFile = '../docs/version.json';
-const databaseFile = '../docs/database.json';
+const versionFile = 'version.json';
+const databaseFile = 'database.json';
 
 const versionKey = 'contentVersion';
 const contentKey = 'contentData';
@@ -26,19 +26,16 @@ async function loadContent(path, hash) {
   let contentData = cachedContent ? JSON.parse(cachedContent) : {};
   const currentHash = contentData.hashes?.[path];
 
-  // If hash matches or content is already in cache, return from cache
   if (currentHash === hash && contentData.data?.[path]) {
     return contentData.data[path];
   }
 
-  // If not in cache or hash is different, fetch new content
   const contentUrl = `${path}/meta.json`;
   try {
     const response = await fetch(contentUrl);
     if (!response.ok) throw new Error('Network response was not ok.');
     const newContent = await response.json();
     
-    // Update the cache
     contentData.hashes = contentData.hashes || {};
     contentData.data = contentData.data || {};
     contentData.hashes[path] = hash;
@@ -54,22 +51,24 @@ async function loadContent(path, hash) {
 
 async function initializeApp() {
   const versionData = await fetchAndCacheVersion();
+  const universityList = document.getElementById('university-list');
+
   if (!versionData) {
-    // Fallback or error handling
-    document.getElementById('university-list').innerHTML = '<p>Failed to load app data. Please try again later.</p>';
+    if (universityList) {
+      universityList.innerHTML = '<p>Failed to load app data. Please try again later.</p>';
+    }
     return;
   }
 
-  const universityDirName = 'nub'; // This should be dynamic in a multi-university setup
+  const universityDirName = 'nub';
   const universityPath = `${contentPath}`;
   const universityHash = versionData.hashes[universityDirName];
   
   const universityContent = await loadContent(universityPath, universityHash);
   if (universityContent) {
     const university = { name: universityDirName, ...universityContent };
-    const universityList = document.getElementById('university-list');
     
-    if (university.children) {
+    if (university.children && universityList) {
       Object.entries(university.children).forEach(([childName, childNode]) => {
         const li = document.createElement('li');
         const a = document.createElement('a');
@@ -83,3 +82,4 @@ async function initializeApp() {
 }
 
 initializeApp();
+
